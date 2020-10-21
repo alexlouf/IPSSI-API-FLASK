55daddb06f73880a22a6f378d48aba48e5a92962
Alexandre Louf <contact@alexandrelouf.fr>
2020-10-21 03:45:25 -0700

    add some forms to control graphic and add some page....

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import json
 import urllib
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
-
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def info():
-    url ="https://api.nomics.com/v1/volume/history?key=demo-26240835858194712a4f8cc0dc635c7a"
+    url = "https://api.nomics.com/v1/volume/history?key=demo-26240835858194712a4f8cc0dc635c7a"
     result = urllib.request.urlopen(url)
-    data =json.load(result)
+    data = json.load(result)
     timestamp = []
     volume = []
     legend = "Volume d'échange quotidien ($)"
@@ -20,18 +20,31 @@ def info():
         volume.append(i['volume'])
     return render_template("index.html", labels=timestamp, values=volume, legend=legend)
 
+
 @app.route("/prix")
 def prix():
-    url = "https://api.nomics.com/v1/exchange-rates/history?key=demo-26240835858194712a4f8cc0dc635c7a&currency=BTC&start=2011-08-18T00%3A00%3A00Z"
+    coin = request.args.get("coin")
+    if coin is None:
+        coin = "BTC";
+
+    start = request.args.get("start")
+    if start is None:
+        start = "2011-08-18";
+
+    end = request.args.get("end")
+    if end is None:
+        end = "2020-12-30";
+    url = "https://api.nomics.com/v1/exchange-rates/history?key=demo-26240835858194712a4f8cc0dc635c7a&currency="+coin+"&start="+start+"T00%3A00%3A00Z&end="+end+"T00%3A00%3A00Z"
     result = urllib.request.urlopen(url)
     data = json.load(result)
     timestamp = []
     price = []
-    legend = "BTC Price"
+    legend = coin+" Price"
     for i in data:
         timestamp.append(i['timestamp'])
         price.append(i['rate'])
-    return render_template("price.html", labels=timestamp, values=price, legend=legend)
+    return render_template("price.html", labels=timestamp, values=price, legend=legend, coin=coin)
+
 
 @app.route("/market")
 def market():
@@ -44,8 +57,18 @@ def market():
     for i in data:
         marketcap.append(i['market_cap'])
         timestamp.append(i['timestamp'])
-    return render_template("market.html" , labels=timestamp, values=marketcap, legend=legend)
+    return render_template("market.html", labels=timestamp, values=marketcap, legend=legend)
 
+
+@app.route("/exchange")
+def exchange():
+    echange = request.args.get("echange")
+    if echange is None:
+        echange = "";
+    url = "https://api.nomics.com/v1/markets?key=demo-26240835858194712a4f8cc0dc635c7a&exchange="+echange+"&base=BTC"
+    result = urllib.request.urlopen(url)
+    data = json.load(result)
+    return render_template("exchange.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
